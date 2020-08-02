b9485ffe65272ea17f51c9d931ed778ec956f00c
Chaokang Wu <chaokanw@usc.edu>
2020-08-02 13:12:40 -0700

    Updates

---
+++ precache-manifest.9a87332fe1a3feb9108bb09b94c2e8b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e5c33ecd77ba1ab62443162775e57a",
+    "url": "/chaokang.space/index.html"
+  },
+  {
+    "revision": "cf8b513dfa598b4a26d9",
+    "url": "/chaokang.space/static/css/main.f6077072.chunk.css"
+  },
+  {
+    "revision": "db113e6863aff309be97",
+    "url": "/chaokang.space/static/js/2.ff5b789f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chaokang.space/static/js/2.ff5b789f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf8b513dfa598b4a26d9",
+    "url": "/chaokang.space/static/js/main.d89e4688.chunk.js"
+  },
+  {
+    "revision": "35c021e08e567cd32924",
+    "url": "/chaokang.space/static/js/runtime-main.8276cabf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chaokang.space/static/media/logo.5d5d9eef.svg"
+  }
+]);
