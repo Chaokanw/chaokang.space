322479758598da80db9e01861850fdd4e47192b2
Chaokang Wu <chaokanw@usc.edu>
2020-08-02 13:04:06 -0700

    Added my project

---
+++ src/MainPage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+class MainPage extends React.Component {
+    render() {
+        return ( 
+            <div>
+            <h1 > Hello, I 'm Chaokang.</h1> 
+            <p class = "heading" >
+                You can also call me Vincent. 
+                </p>
+
+            <p >
+                I am a student at the University of Southern California.
+                I write &nbsp; 
+                <a class = "highlight" href = "#">code</a> &nbsp;
+                and take &nbsp; 
+                <a class = "highlight" href = "#" >photos</a> &nbsp;. 
+            </p>
+
+            </div>
+        )
+    }
+
+}
+
+export default MainPage
