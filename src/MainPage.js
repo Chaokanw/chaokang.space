f1fc71b17470a6eb30c2c9336140909fe2f72e5a
Chaokang Wu <chaokanw@usc.edu>
2020-08-02 15:44:10 -0700

    add cname

--- src/MainPage.js
+++ src/MainPage.js
@@ -5,15 +5,19 @@ class MainPage extends React.Component {
     render() {
         return ( 
             <div>
-            <h1 > Hello, I 'm Chaokang.</h1> 
-            <p class = "heading" >
-                You can also call me Vincent. 
+                <h1 > Hello, I 'm Chaokang.</h1> 
+                <p class = "heading" >
+                    You can also call me Vincent. 
+                    </p>
+
+                <p >
+                    Sorry! This site is currently under construction. 
                 </p>
 
-            <p >
-                Sorry! This site is currently under construction. 
-                Please check back later.
-            </p>
+                <p>
+                    Please check back later. Thank you!
+                </p>
+
 
             </div>
         )
