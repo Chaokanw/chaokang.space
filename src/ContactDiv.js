819d5c0ff6c16aaa9884e5a25e50a5afa2bc49cf
Chaokang Wu <chaokanw@usc.edu>
2020-08-23 21:21:41 -0700

    main page update

---
+++ src/ContactDiv.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+
+let contactInfo = {
+    "linkedin": "https://www.linkedin.com/in/chaokang-wu/",
+    "github": "https://github.com/Chaokanw",
+    "facebook":"https://www.facebook.com/profile.php?id=100004086908396",
+    "email":"chaokang.wu@hotmail.com"
+}
+
+class ContactDiv extends React.Component {
+    render() {
+        return ( 
+            <div id="contact-div" target="_blank">
+                <a href={contactInfo.linkedin} target="new">
+                    <i className="fab fa-linkedin"></i>
+                </a>
+
+                <a href={contactInfo.github} target="new">
+                    <i className="fab fa-github-square"></i>  
+                </a>
+
+                <a href={contactInfo.facebook} target="new">
+                    <i className="fab fa-facebook-square"></i>
+                </a>
+
+                <a href={"mailto:"+contactInfo.email} target="new">
+                    <i className="fas fa-envelope-square"></i>
+                </a>
+            </div>
+        )
+    }
+
+}
+
+export default ContactDiv
--- src/MainPage.js
+++ src/MainPage.js
@@ -1,22 +1,39 @@
 import React from 'react';
+import ContactDiv from './ContactDiv'
+
+
 
 
 class MainPage extends React.Component {
     render() {
         return ( 
             <div>
-                <h1 > Hello, I 'm Chaokang.</h1> 
-                <p class = "heading" >
+                <h1> Hello, I 'm <span className="highlight yellow">Chaokang</span>.</h1> 
+                <p className = "heading" >
                     You can also call me Vincent. 
                     </p>
 
-                <p >
-                    Sorry! This site is currently under construction. 
+                <p>
+                    I'm a graduate student at the&nbsp;
+                    <a id="USC" className="highlight" href="https://www.usc.edu/" target="new">USC</a>, 
+                    majoring in computer science. Currently, I'm actively seeking a 
+                    full-time <span className="highlight pink">software&nbsp;engineering</span> position.
                 </p>
 
                 <p>
-                    Please check back later. Thank you!
+                    You can find some of my project demos <a href="#" className="highlight blue">here</a>:
                 </p>
+
+                <p>
+                    Outside of programming, I also enjoy travel, skiing, editing videos, and taking photos  
+                </p>
+
+
+                <p>
+                    Please feel free to contact me in the following ways:
+                </p>
+                <ContactDiv>
+                </ContactDiv>
 
 
             </div>
