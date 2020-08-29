4e5445c3da7b0aa774b59af61f213ed9588830db
Chaokang Wu <chaokanw@usc.edu>
2020-08-29 16:01:27 -0700

    fixed refresh 404 bug3

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,12 @@
 /* config-overrides.js */
 module.exports = function override(config, env) {
     //do stuff with the webpack config...
-    devServer: {
+
+    // config.output = {
+    //     publicPath:'/'
+    // }
+
+    config.devServer = {
         historyApiFallback:{
             index:'build/index.html'
         }
